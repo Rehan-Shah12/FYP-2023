9c771fe18d9c01960b1f4afd5628fc47cf91c64b
Rehan-Shah12 <100332064+Rehan-Shah12@users.noreply.github.com>
2023-05-05 16:15:30 +0500

    Faster Scraping and only 10 items

--- server.js
+++ server.js
@@ -8,9 +8,13 @@ import productRoutes from "./routes/productRoute.js";
 import scraperRoutes from "./routes/scraperRoute.js";
 import scraper from "./scraper.js";
 import cors from "cors";
-
+import { fileURLToPath } from "url";
+import path from "path";
 dotenv.config();
 const PORT = process.env.PORT || 8080;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 //Databse Config
 connectDB();
@@ -19,6 +23,7 @@ connectDB();
 const app = express();
 app.use(express.json());
 app.use(cors());
+app.use(express.static(path.join(__dirname, "./client/build")));
 
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/category", categoryRoutes);
@@ -26,8 +31,8 @@ app.use("/api/v1/product", productRoutes);
 app.use("/api/v1/scraper", scraperRoutes);
 app.use("/api/v1/scraper", scraper);
 
-app.get("/", (req, res) => {
-  res.send("<h1>Rehan Shah</h1>");
+app.use("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
 app.listen(PORT, () => {
